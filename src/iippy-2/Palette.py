e4bdab34606d53cd6b9a62601f148df7e1eb719b
kidult00 <sysulj@gmail.com>
2015-04-02 20:16:43 +0800

    update

--- src/iippy-2/Palette.py
+++ src/iippy-2/Palette.py
@@ -5,8 +5,12 @@ import simplegui
 import math
 
 # intialize globals
-ball_pos = [250,250]
+circle_pos = [250,250]
+triangle_1 = [0,0]
+triangle_2 = [0,0]
+triangle_3 = [0,0]
 BALL_RADIUS = 15
+tri_height = 10
 
 
 # helper function
@@ -14,11 +18,20 @@ BALL_RADIUS = 15
 # define event handlers
 
 def mouseclick(pos):
-	global ball_pos
-    ball_pos = list(pos)
+    global circle_pos,triangle_pos,triangle_1,triangle_2,triangle_3
+    circle_pos = list(pos)
+    x = pos[0]
+    y = pos[1]
+    triangle_1 = [x, y-tri_height]
+    triangle_2 = [x+2*tri_height,y+tri_height]
+    triangle_3 = [x-2*tri_height,y+tri_height]
+    print "c=",circle_pos
+#    print "t=",triangle_pos
 
 def draw(canvas):
-	canvas.draw_circle(ball_pos, BALL_RADIUS, 1, "Black")
+    canvas.draw_circle(circle_pos, BALL_RADIUS, 1, "Black")
+    canvas.draw_polygon([triangle_1,triangle_2,triangle_3], 1, "Black")
+
 
 # create frame and controls
 frame = simplegui.create_frame("Palette", 500, 500)
