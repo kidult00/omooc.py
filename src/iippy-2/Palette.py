36394147508595aec1e01c3481796d9f12b3c6ea
kidult00 <sysulj@gmail.com>
2015-04-03 08:19:13 +0800

    Palette progress

--- src/iippy-2/Palette.py
+++ src/iippy-2/Palette.py
@@ -5,33 +5,53 @@ import simplegui
 import math
 
 # intialize globals
-circle_pos = [250,250]
-triangle_1 = [0,0]
-triangle_2 = [0,0]
-triangle_3 = [0,0]
-BALL_RADIUS = 15
-tri_height = 10
+ShapeType = "circle"
+Radius = 15
+shape_list = []
 
 
 # helper function
      
 # define event handlers
 
+def draw_circle():
+    global ShapeType
+    ShapeType = "circle"
+
+def draw_triangle():
+    global ShapeType
+    ShapeType = "triangle"
+
+def draw_square():
+    global ShapeType
+    ShapeType = "square"
+
+
 def mouseclick(pos):
-    global circle_pos,triangle_pos,triangle_1,triangle_2,triangle_3
-    circle_pos = list(pos)
+    global ShapeType,shape_list,Radius
+    
     x = pos[0]
     y = pos[1]
-    triangle_1 = [x, y-tri_height]
-    triangle_2 = [x+2*tri_height,y+tri_height]
-    triangle_3 = [x-2*tri_height,y+tri_height]
-    print "c=",circle_pos
-#    print "t=",triangle_pos
+
+    if ShapeType == "circle":
+        pos = [x,y]
+    elif ShapeType == "triangle":
+        pos = [(x, y - Radius),(x + 2*Radius, y + Radius),(x - 2*Radius, y + Radius)]
+    elif ShapeType == "square":
+        pos = [(x - Radius , y - Radius),(x + Radius , y - Radius),(x + Radius , y + Radius),(x - Radius , y + Radius)]
+
+    print pos
+
+    shape_list.append([pos,ShapeType])
 
 def draw(canvas):
-    canvas.draw_circle(circle_pos, BALL_RADIUS, 1, "Black")
-    canvas.draw_polygon([triangle_1,triangle_2,triangle_3], 1, "Black")
-
+    global ShapeType,shape_list,Radius
+    for shapes in shape_list:
+        if shapes[1] == "circle":
+            canvas.draw_circle(shapes[0],Radius, 1, "Black")
+        else:
+            canvas.draw_polygon(shapes[0], 1, "Black")
+ 
 
 # create frame and controls
 frame = simplegui.create_frame("Palette", 500, 500)
@@ -41,7 +61,9 @@ frame.set_canvas_background("White")
 # register event handlers
 frame.set_mouseclick_handler(mouseclick)
 frame.set_draw_handler(draw)
-
+frame.add_button("圆形\n",draw_circle,100)
+frame.add_button("三角形\n",draw_triangle,100)
+frame.add_button("正方形\n",draw_square,100)
 
 # get things rolling
 frame.start()
