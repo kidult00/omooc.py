6809cbaf69b54c6b6565866d7d0be968cf1f479b
kidult00 <sysulj@gmail.com>
2015-04-11 18:59:58 +0800

    v1 update,add list

--- src/iippy-3/GuessTheNumber-AI.py
+++ src/iippy-3/GuessTheNumber-AI.py
@@ -1,6 +1,3 @@
-# template for "Guess the number" mini-project
-# input will come from buttons and an input field
-# all output for the game will be printed in the console
 
 #import simplegui
 try:
@@ -11,13 +8,14 @@ except ImportError:
 import random
 import math
 
-num_max = 10
-num_min = 0
 secret_number = 0
 guess_number = 0
+num_max = 10
+num_min = 0
 result = ''
+guess_list = []
 
-# helper function to start and restart the game
+# helper function to start and restart the game 
 # def new_game():
 #     # initialize global variables used in your code here
 #     global secret_number,result
@@ -33,28 +31,32 @@ def range100():
     global secret_number,num_max,num_min
     secret_number = random.randrange(0, 10)
     print "New game? Welcome~"
-    print ""
+    result = "start"
     num_max = 10
     num_min = 0
     guess_number = 0
+    guess_list = []
     
-#for test
-    print "secret_number is:",secret_number 
+    
+    print "secret_number is:",secret_number      #for test
 
 
     
 def ai_num():
-    global num_max,num_min
-    newAI = (num_max + num_min)/2
-
-    print "new ai:",newAI
+    global num_max,num_min,guess_list
+    newAI = (num_max + num_min)/2   # get the new num between secret and guess
+    guess_list.append(newAI)
+    print "new ai:",newAI   #for test
+    print guess_list
+    print "You've guessed "+str(len(guess_list))+" times"
     return newAI
     
 
 def compare():
-    global secret_number,guess_number,num_max,num_min,result
+    global secret_number,guess_number,num_max,num_min,result,guess_list
     guess_number = ai_num()
     print "Your guess was: ",guess_number
+    
     if secret_number > guess_number :
          result = 'low'
          num_min = guess_number     # set the new min
@@ -63,6 +65,9 @@ def compare():
          num_max = guess_number     # set the new max
     else:
          result = 'bingo'
+         
+         #for i in range(len(guess_list)):
+          #   guess_list.pop(i)
     
     return result
 
@@ -74,6 +79,7 @@ def draw(canvas):
         canvas.draw_text("Nope~ Should be a lower one." ,(10,15),12,"black")
     elif result == 'bingo':
         canvas.draw_text("\(^o^)/~ Bingo!!" ,(10,15),12,"black")
+        canvas.draw_text("Your guess history:"+str(guess_list) ,(10,30),12,"black")
     else:
         canvas.draw_text("Welcome~" ,(10,15),12,"black")
     
